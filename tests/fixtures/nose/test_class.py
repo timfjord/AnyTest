a7f38599949d5530ac00e0c1602ba73d1a347ced
Tim Masliuchenko <timfjord@hey.com>
2022-02-08 11:56:38 +0000

    Exclude tests/fixtures from linting

--- tests/fixtures/nose/test_class.py
+++ tests/fixtures/nose/test_class.py
@@ -2,22 +2,18 @@ class TestNumbers:
     def test_numbers(self):
         assert 1 == 1
 
-
-class TestSubclass(Subclass):  # noqa
+class TestSubclass(Subclass):
     def test_subclass(self):
         assert 1 == 1
 
-
-class Test_underscores_and_123(Subclass):  # noqa
+class Test_underscores_and_123(Subclass):
     def test_underscores(self):
         assert 1 == 1
 
-
-class UnittestClass(unittest.TestCase):  # noqa
+class UnittestClass(unittest.TestCase):
     def test_unittest(self):
         assert 1 == 1
 
-
-class SomeTest(TestCase):  # noqa
+class SomeTest(TestCase):
     def test_foo(self):
-        foo = date(2017, 11, 16)  # noqa
+        foo = date(2017, 11, 16)
--- tests/fixtures/nose/test_method.py
+++ tests/fixtures/nose/test_method.py
@@ -1,14 +1,12 @@
-# fmt: off
-
+# comment
 
 def test_numbers():
     assert 1 == 1
-
 
 def test_foo():
     class CustomException(Exception):
         pass
 
-    mocker.patch('some.module', side_effect=CustomException())  # noqa
+    mocker.patch('some.module', side_effect=CustomException())
 
-    assert something # noqa
+    assert 'something'

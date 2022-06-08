c5e7ba23a80d632d9fc442982873d10e8017f032
Tim Masliuchenko <timfjord@hey.com>
2022-06-08 14:16:53 +0100

    Make assertLastCommand smarter

--- tests/test_frameworks/ruby/test_cucumber.py
+++ tests/test_frameworks/ruby/test_cucumber.py
@@ -6,11 +6,11 @@ class CucumberTestCase(SublimeProjectTestCase):
 
     def test_line(self):
         yield from self._testFile(('features', 'normal.feature'), 1)
-        self.assertLastCommand('cucumber features/normal.feature:1')
+        self.assertLastCommand('cucumber ', ('features', 'normal.feature'), ':1')
 
     def test_file(self):
         yield from self._testFile(('features', 'normal.feature'))
-        self.assertLastCommand('cucumber features/normal.feature')
+        self.assertLastCommand('cucumber ', ('features', 'normal.feature'))
 
     def test_suite(self):
         yield from self._testSuite(('features', 'normal.feature'))

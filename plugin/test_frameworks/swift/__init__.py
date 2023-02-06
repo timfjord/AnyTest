8069822f329f5f21b97de63b6c2854d9c9365046
Tim Masliuchenko <timfjord@hey.com>
2023-02-06 17:40:06 +0000

    Add swift test frameworks

---
+++ plugin/test_frameworks/swift/__init__.py
@@ -0,0 +1,10 @@
+from .. import TestFramework as BaseTestFramework
+
+
+class TestFramework(BaseTestFramework):
+    language = 'swift'
+    test_patterns = (r'^\s*func (test.*)\(\)',)
+    namespace_patterns = (
+        r'^(?:(?:public )?(?:final )?|(?:final )?(?:public )?)class ([-_a-zA-Z0-9]+): XCTestCase',
+    )
+    module_patterns = (r'^Tests[/\\]([-_ a-zA-Z0-9]+)(?:[/\\]|\.swift)',)
